ececeed7830ce5a057af087f2be8df347fb444fc
GadgetLumic <the-komet-gryphon@libero.it>
2017-01-23 21:21:38 +0100

    Update and rename .gitignore to Test.py

---
+++ Test.py
@@ -0,0 +1,49 @@
+#encoding=utf-8
+
+import asyncio
+
+import aiohttp
+
+from aiohttp import web
+
+import plugins
+
+from lumicbot import User
+
+
+
+@asyncio.coroutine
+
+def send_msg(msg):
+
+    print(msg)
+
+
+
+@asyncio.coroutine
+
+def init(loop):
+
+    print("Testing mode! Type to simulate group chat messages.")
+
+    while True:
+
+        line = input("> ")
+
+        for plugin in plugins.plugins:
+
+            yield from plugin.handle_event(line, User(0, "$User", "$Lastname", "@username") , is_group=True, callback=send_msg)
+
+
+
+loop = asyncio.get_event_loop()
+
+loop.run_until_complete(init(loop))
+
+try:
+
+    loop.run_forever()
+
+except KeyboardInterrupt:
+
+    pass
